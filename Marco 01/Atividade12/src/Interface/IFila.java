7356b00425ddec55152fa9322386cabcbfdce93b
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2023-05-27 10:15:40 -0300

    12

---
+++ Marco 01/Atividade12/src/Interface/IFila.java
@@ -0,0 +1,10 @@
+package Interface;
+
+public interface IFila {
+
+    public boolean adiciona(Object info);
+    public boolean remove();
+    public boolean estaVazia();
+    public int tamanho();
+    
+}
