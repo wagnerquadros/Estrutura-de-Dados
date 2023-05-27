e369fde91f1a431559a15c56e8280b23fc86c2a5
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2023-05-27 10:40:18 -0300

    Atividade12

--- Marco 01/Atividade12/src/App.java
+++ Marco 01/Atividade12/src/App.java
@@ -1,5 +1,26 @@
+import Objetos.Aluno;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Aluno a1 = new Aluno("Albert", 17, 9);
+        Aluno a2 = new Aluno("Robert", 17, 9);
+        Aluno a3 = new Aluno("Enrico", 17, 9);
+        Aluno a4 = new Aluno("Eugene", 17, 9);
+        Aluno a5 = new Aluno("Hans", 17, 9);
+
+        FilaLista fila = new FilaLista();
+        fila.adiciona(a1);
+        fila.adiciona(a2);
+        fila.adiciona(a3);
+        fila.adiciona(a4);
+        fila.adiciona(a5);
+        fila.imprimirFilaLista();
+        System.out.println(fila.tamanho());
+        System.out.println("");
+
+        fila.remove();
+        fila.imprimirFilaLista();
+        System.out.println(fila.tamanho());
     }
 }
---
+++ Marco 01/Atividade12/src/FilaLista.java
@@ -0,0 +1,66 @@
+import Interface.IFila;
+
+public class FilaLista implements IFila {
+
+    private Noh inicio;
+    private Noh fim;
+
+    public FilaLista(){
+        this.inicio = null;
+        this.fim = null;
+    }
+
+    public boolean adiciona(Object info) {
+        Noh novo = new Noh(info);
+        if (estaVazia())
+            inicio = novo;
+        else
+            fim.setProximo(novo);
+        fim = novo;
+        return true;
+    }
+
+    public boolean remove() {
+        if(!estaVazia()){
+            if(inicio == fim){
+                inicio = null;
+                fim = null;
+            } else {
+                inicio = inicio.getProximo();
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean estaVazia() {
+        if(inicio != null) 
+            return false;
+        return true;
+    }
+
+   
+    public int tamanho() {
+        Noh aux = inicio;
+        int tamanho = 0;
+
+        if (!this.estaVazia()){
+            while(aux!=null){
+                tamanho +=1;
+                aux = aux.getProximo();
+            }
+        } 
+        return tamanho;
+    }
+
+
+    public void imprimirFilaLista(){
+        Noh aux = inicio;
+        while (aux != null){
+            System.out.println(aux.getInfo().toString());
+            aux = aux.getProximo();
+        }
+    }
+    
+}
